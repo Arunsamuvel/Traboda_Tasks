d7bb0fd95ff1327434fcbb486d2e5ac3b7727cbd
Arunsamuvel <arun15ee03@gmail.com>
2024-11-18 23:30:45 +0530

    tailwind setup completed

--- kanban-board/src/App.js
+++ kanban-board/src/App.js
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      <h1>
-        Hello World!
-      </h1>
+      <h1 className="text-red-600 text-4xl font-[arial]">BRio </h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
