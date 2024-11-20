a7d4daed08496b3a1635e6b870f518d661cb6858
Arunsamuvel <arun15ee03@gmail.com>
2024-11-20 23:58:32 +0530

    learn draggable component

--- kanban-board/src/App.js
+++ kanban-board/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Logo from "./component/Logo/Logo";
+import Draggable from "react-draggable";
 
 const App = () => {
   return (
     <div className="font-[poppins] bg-gradient-to-t from-[#77CDFF] to-[#FFFFFF] h-screen  w-screen">
       <Logo />
+      
+      <Draggable>
+        <div className="border border-red-900 w-32">
+          I can now be moved around!
+        </div>
+      </Draggable>
     </div>
   );
 };
