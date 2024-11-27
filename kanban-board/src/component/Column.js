2dbef5121f4b71829c90409ad68674770e941457
Arunsamuvel <arun15ee03@gmail.com>
2024-11-27 23:40:56 +0530

    folder structure

--- kanban-board/src/component/Column.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Droppable } from "react-beautiful-dnd";
-
-const Column = () => {
-  return <div></div>;
-};
-
-export default Column;
--- kanban-board/src/component/Task.js
+++
@@ -1,8 +0,0 @@
-// src/Task.js
-import React from "react";
-
-const Task = ({ task }) => {
-  return <div className="bg-white rounded shadow p-2 mb-2">{task.content}</div>;
-};
-
-export default Task;
